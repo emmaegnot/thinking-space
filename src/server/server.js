60b849252ccfcca1be195282d1514695f89cc63d
emmaegnot <bb23833@bristol.ac.uk>
2024-10-30 11:06:42 +0000

    first basic webpage-server complete
    get the webpage to link with the server
    styling is linked to webpage

--- src/server/server.js
+++ src/server/server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const path = require('path')
 
+
 const app = express();
 const port = 3000;
 
 
-app.set('view engine','html');
+app.set('view engine','ejs');
+app.set('views', path.join(__dirname, '../views'))
+
+app.use(express.static(path.join(__dirname,'../public')));
 
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname,'../public/index.html'));
+    res.render('index');
 });
 
 app.listen(port, () => {
