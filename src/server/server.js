22da3191c370200656f3c3ee2cad7840e3c1af86
ethanr04 <na23961@bristol.ac.uk>
2024-11-06 11:49:55 +0000

    Removed unnecessary borders, added highlights to hovering over an image

--- src/server/server.js
+++ src/server/server.js
@@ -12,7 +12,7 @@ app.set('views', path.join(__dirname, '../views'))
 app.use(express.static(path.join(__dirname,'../public')));
 
 app.get('/', (req,res) => {
-    res.render('index');
+    res.render('choose_shape');
 });
 
 app.listen(port, () => {
