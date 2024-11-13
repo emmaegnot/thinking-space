826c01cfbc89b14a16302b24034c131641290900
Ethan Rogers <157602884+ethanr04@users.noreply.github.com>
2024-11-13 17:21:05 +0000

    Update server.js
    Not sure why this wasn't updated when I pushed as I did add the file. Keeps original server.js functionality, and comments out testing logic

--- src/server/server.js
+++ src/server/server.js
@@ -11,25 +11,27 @@ app.set('views', path.join(__dirname, '../views'))
 
 app.use(express.static(path.join(__dirname,'../public')));
 
-// app.get('/', (req,res) => {
-//     res.render('index');
-// });
+app.get('/', (req,res) => {
+    res.render('index');
+});
 
 app.get('/choose_shape', (req,res) => {
     res.render('choose_shape');
 });
 
-//PLACEHOLDER - Allows for testing of additional_words
-app.get('/', (req,res) => {
-    // Testing variables for now, server will do this in the future
-    const colour = {
-        r: 255,
-        g: 0,
-        b: 0,
-    }
-    wordList = ['Irritated', 'Resentful', 'Miffed', 'Upset', 'Mad', 'Furious', 'Raging', 'Hot']
-    res.render('additional_words', {filepath: "images/star.png", colour, wordList});
-});
+//PLACEHOLDER - Allows for testing of additional_words, must comment out lines 14-16
+// app.get('/', (req,res) => {
+//// Testing variables for now, server will do this in the future
+//     const colour = {
+//         r: 255,
+//         g: 0,
+//         b: 0,
+//     }
+//     // The words should be computed by server, then sent to a request like this (ideally)
+//     // I.e. in this case the original word picked was angry
+//     wordList = ['Irritated', 'Resentful', 'Miffed', 'Upset', 'Mad', 'Furious', 'Raging', 'Hot']
+//     res.render('additional_words', {filepath: "images/star.png", colour, wordList});
+// });
 
 app.post('/submit-shape', (req,res) => {
     var selectedShape = req.shape
