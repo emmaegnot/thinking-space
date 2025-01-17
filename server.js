bb7e35e1731dcf217dcc5e507420b2719db8b7ca
anni <159738393+Strawbanni@users.noreply.github.com>
2025-01-17 21:05:45 +0000

    Feeling force bug fixes and updated header
    Fixed reported issues on Feeling force where:
    "Feeling force" text font now matches other pages.
    Arrow only rotates 180 degrees and will stay within limit of meter.
    Arrow is no longer positioned with respect to window and will not change for different browser window sizes.
    Page is placed before choose_word and after choose_colour
    Added get/post for submit-force
    
    Additional enhancements:
    The max and min Force values, given by clickCount, is 8 and -8 according to the number of times arrow can be clicked within the 180 limit.
    Container shape width changed to fit the page better.
    
    Header:
    Made a start on minor changes to stylesheet to change shape, font and colour of the header. Will build on this next as the design is not final.

---
+++ server.js
@@ -0,0 +1,189 @@
+const express = require('express');
+const session = require('express-session'); //npm install express-session
+const cookieParser = require('cookie-parser');
+const path = require('path')
+
+
+const app = express();
+app.use(express.urlencoded({extended:true}))
+const port = 3000;
+const shapes = {
+    cloud: ['friendly', 'comfortable', 'happy', 'dreamy'],
+    circle: ['calm', 'friendly', 'connected'],
+    square: ['stable', 'calm', 'confused'],
+    parallelogram: ['unstable'],
+    hexagon: ['connected'],
+    star: ['excited', 'dreamy'],
+    triangle: ['angry', 'concerned', 'scared'],
+    spikeyball: ['irritated', 'scared', 'isolated']
+};
+const colours = {
+    red: ['angry', 'scared'],
+    green: ['calm'],
+    yellow: ['happy'],
+    blue: ['dreamy'],
+    black: ['scared', 'isolated']
+}
+const words = {
+    Angry: ['angry', 'concerned', 'confused', 'irritated'],
+    Disgusted: ['irritated'],
+    Fearful: ['scared', 'confused'],
+    Happy: ['happy', 'excited', 'dreamy'],
+    Sad: ['sad', 'concerned'],
+    Surprise: ['concerned', 'exited', 'confused', 'happy'],
+
+}
+// Find shared associations among word, shape and colour
+function getSharedWords(shape, colour, word) {
+    // Fetch words associated with the shape, colour, and word
+    const shapeWords = shapes[shape] || [];
+    const colourWords = colours[colour] || [];
+    const wordWords = words[word] || [];
+
+    // Create an array combining all words
+    const combinedWords = [...shapeWords, ...colourWords, ...wordWords];
+
+    // Create a frequency map for the words
+    const wordCount = {};
+    combinedWords.forEach(w => {
+        if (w) {
+            wordCount[w] = (wordCount[w] || 0) + 1;
+        }
+    });
+
+    // Filter for common words that appear in at least two categories
+    const commonWords = Object.keys(wordCount).filter(word => wordCount[word] > 1);
+
+    // If no common words are found, return "indecisive"
+    if (commonWords.length === 0) {
+        return ['indecisive'];
+    }
+
+    // Find the most frequent words from the common words
+    let maxCount = 0;
+    const mostFrequentWords = [];
+
+    commonWords.forEach(word => {
+        const count = wordCount[word];
+        if (count > maxCount) {
+            maxCount = count;
+            mostFrequentWords.length = 0; // Clear the array
+            mostFrequentWords.push(word);
+        } else if (count === maxCount) {
+            mostFrequentWords.push(word);
+        }
+    });
+
+    // Return the most frequent shared words
+    return mostFrequentWords;
+}
+
+
+
+app.set('view engine','ejs');
+app.set('views', path.join(__dirname, '../views'))
+
+app.use(session({
+    secret: /*process.env.SESSION_SECRET ||*/ 'we_should_come_up_with_a_key',
+    resave: false,
+    saveUninitialized: true,
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname,'../public')));
+app.use(express.urlencoded({extended:true}))
+
+
+app.get('/', (req,res) => {
+    // Check if the consent cookie exists
+    if (!req.cookies.consent) {
+        // Send HTML with a cookie consent banner
+        res.send(`
+            <h1>Welcome to Our Website</h1>
+            <p>We use cookies to enhance your experience. Do you accept?</p>
+            <button id="accept">Accept</button>
+            <script>
+                document.getElementById('accept').onclick = function() {
+                    document.cookie = "consent=true; path=/; max-age=" + 60*60*24*30; // 30 days
+                    location.reload();
+                };
+            </script>
+        `);
+    } else {
+        res.render('index');
+    }
+
+});
+
+app.get('/choose_shape', (req,res) => {
+    res.render('choose_shape');
+});
+
+//PLACEHOLDER - Allows for testing of additional_words, must comment out lines 14-16
+// app.get('/', (req,res) => {
+//// Testing variables for now, server will do this in the future
+//     const colour = {
+//         r: 255,
+//         g: 0,
+//         b: 0,
+//     }
+//     // The words should be computed by server, then sent to a request like this (ideally)
+//     // I.e. in this case the original word picked was angry
+//     wordList = ['Irritated', 'Resentful', 'Miffed', 'Upset', 'Mad', 'Furious', 'Raging', 'Hot']
+//     res.render('additional_words', {filepath: "images/star.png", colour, wordList});
+// });
+
+app.post('/submit-shape', (req,res) => {
+    req.session.shape = req.body.shape
+    var filePath = "images/"
+    req.session.filePath = filePath.concat(req.session.shape, ".png")
+    res.redirect('/choose_colour');
+})
+
+app.get('/choose_colour', (req,res) => {
+    res.render('choose_colour', {filepath: req.session.filePath});
+});
+
+app.post('/submit-colour', (req, res) => {
+    req.session.colour = req.body.colour;  
+    res.redirect('/feeling_force');          
+});
+
+app.get('/feeling_force', (req,res) => {
+    res.render('feeling_force');
+});
+
+app.post('/submit-force', (req, res) => {
+    req.session.colour = req.body.colour;  
+    res.redirect('/choose_word');          
+});
+
+app.get('/choose_word', (req,res) => {
+    res.render('choose_word');
+});
+
+app.post('/submit-word', (req, res) => {
+    req.session.word = req.body.selectedEmotion;
+     // Save mood in session
+    res.redirect('/mood_summary');     // Redirect to mood summary page
+});
+
+app.get('/mood_summary', (req,res) => {
+    const shape = req.session.shape;
+    const colour = req.session.colour;
+    const word = req.session.word;
+    console.log(word)
+    const potentialMoods = getSharedWords(shape, colour, word)
+    //Gets associations between all of the choicees
+    let mood;
+    const randomIndex = Math.floor(Math.random() * potentialMoods.length)
+    mood = potentialMoods[randomIndex]
+
+    req.session.mood = mood;
+    res.render('mood_summary', {mood: req.session.mood});
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
