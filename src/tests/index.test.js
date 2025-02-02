ef3be7262fe51701f2531c745436727a64914846
ethanr04 <na23961@bristol.ac.uk>
2025-02-02 17:27:48 +0000

    Added index tests to test cookie popup behaviour

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -7,6 +7,20 @@ test("Checks page is rendered with the correct title", async () => {
     expect(res.text).toContain("<title>The Thinking Space | Home</title>");
 });
 
+test("Checks that cookie consent popup appears when no consent cookie is present", async () => {
+    const res = await request(app).get("/");
+    expect(res.text).toContain("const showPopup = true");
+    expect(res.text).toContain('<div id="cookiePopup"');
+  });
+
+test("should NOT show the cookie consent popup when the consent cookie is set", async () => {
+    const res = await request(app)
+    .get("/")
+    .set("Cookie", ["consent=true"]);
+    expect(res.text).toContain("const showPopup = false");
+    expect(res.text).not.toContain('<div id="cookiePopup"');
+  });
+
 afterAll(() => {
     server.close(); // Close the server after the tests are done
   });
