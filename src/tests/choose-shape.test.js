6eb7332ed5a41db8d3e3957fb04827b19e126e7d
ethanr04 <na23961@bristol.ac.uk>
2025-02-05 10:43:11 +0000

    Added first test to request page and check title

--- src/tests/choose-shape.test.js
+++ src/tests/choose-shape.test.js
@@ -1,6 +1,12 @@
 const request = require("supertest");
 const {server, app} = require("../server/server");
 
+test("Checks page is rendered with the correct title", async () => {
+    const res = await request(app).get("/choose_shape");
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toContain("<title>The Thinking Space | Choose A Shape</title>");
+});
+
 afterAll(() => {
     server.close(); // Close the server after the tests are done
   });
