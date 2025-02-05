fb7da113b21cbe2c3e465968e652ebd15fa8822e
ethanr04 <na23961@bristol.ac.uk>
2025-02-05 10:49:11 +0000

    Added test in both choose-shape and index test.js files to check the icon is included

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,10 +1,11 @@
 const request = require("supertest");
 const {server, app} = require("../server/server");
 
-test("Checks page is rendered with the correct title", async () => {
+test("Checks page is rendered with the correct title and icon", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toBe(200);
     expect(res.text).toContain("<title>The Thinking Space | Home</title>");
+    expect(res.text).toContain('<link rel="icon" href="/images/icon.png" sizes="64x64"></link>')
 });
 
 test("Checks that cookie consent popup appears when no consent cookie is present", async () => {
