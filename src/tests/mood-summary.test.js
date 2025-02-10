7bf6a7dbf9cd4148790b5d528f089d18edc03c54
ethanr04 <na23961@bristol.ac.uk>
2025-02-10 13:23:39 +0000

    Add test to check that the page renders with a mood

--- src/tests/mood-summary.test.js
+++ src/tests/mood-summary.test.js
@@ -2,16 +2,21 @@ const request = require("supertest");
 const {server, app} = require("../server/server");
 
 test("Checks page is rendered with the correct title and icon", async () => {
-    // const agent = request.agent(app); // Creates a persistent session
-    // await agent.post('/next-shape').send({ shape: 'circle' }); // Choose shape
-    // await agent.post('/next-colour').send({ colour: 'blue' }); // Choose colour
-    // await agent.post('/next-word').send({ selectedEmotion: 'Happy' }); // Choose word
-    // await agent.post('/submit-force').send({ clickCount: 7 }); // Choose force
-    // const res = await agent.get('/mood_summary'); // Get mood summary
     const res = await request(app).get("/mood_summary");
     expect(res.status).toBe(200); // Status code 200 indicates a successful request and response
     expect(res.text).toContain("<title>The Thinking Space | Mood Summary</title>"); // Checks dynamic title
 });
+
+test("Checks page is rendered with a mood", async () => {
+    const agent = request.agent(app); // Creates a persistent session
+    await agent.post('/next-shape').send({ shape: 'circle' }); // Choose shape
+    await agent.post('/next-colour').send({ colour: 'blue' }); // Choose colour
+    await agent.post('/next-word').send({ selectedEmotion: 'Happy' }); // Choose word
+    await agent.post('/submit-force').send({ clickCount: 7 }); // Choose force
+    const res = await agent.get('/mood_summary'); // Get mood summary
+    expect(res.status).toBe(200); // Status code 200 indicates a successful request and response
+    expect(res.text).toContain("<h2 class=\"colouredText\">Are you feeling indecisive </h2>"); // Checks the server assigns a mood
+})
 
 afterAll(() => {
     server.close(); // Close the server after the tests are done
