6d81a91fe652c587b6ecb219e1add9e285afbd0e
ethanr04 <na23961@bristol.ac.uk>
2025-02-11 19:45:15 +0000

    Added test to check that all words appear as selectable buttons

--- src/tests/choose-word.test.js
+++ src/tests/choose-word.test.js
@@ -4,7 +4,7 @@ const {server, app} = require("../server/server");
 test("Checks page is rendered with the correct title and icon", async () => {
     const res = await request(app).get("/choose_word") // Make request to server
     expect(res.statusCode).toBe(200); // Status code 200 indicates a successful request and response
-    expect(res.text).toContain("<title>The Thinking Space | Choose A Colour</title>"); // Checks dynamic title
+    expect(res.text).toContain("<title>The Thinking Space | Choose A Word</title>"); // Checks dynamic title
     expect(res.text).toEqual(expect.stringContaining('<link rel="icon" href="/images/icon.png" sizes="64x64">')); // Check icon is included in response
 });
 
@@ -21,6 +21,15 @@ test("Checks that the form submits to /next-word when NEXT is clicked", async ()
 test("Checks that clicking BACK submits to /previous-word", async () => {
     const res = await request(app).get("/choose_word");
     expect(res.text).toContain("form.action = '/previous-word';"); // The response should contain code for going back to the choose shape page
+});
+
+test("Checks if the word selection buttons are present", async () => {
+    const res = await request(app).get("/choose_word");
+    // Checks that each emotion is able to be selected as a button
+    const emotions = ["Angry", "Disgusted", "Fearful", "Happy", "Sad", "Surprised"];
+    emotions.forEach(emotion => {
+        expect(res.text).toContain(`<button type="button" name="emotion" value="${emotion}" onclick="highlightButton(this)">`); // For each emotion, check the response contains an input field with its name
+    });
 });
 
 test("Checks footer contains the logo and motto", async () => {
