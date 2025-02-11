4dba0c944ac293e0916a0ba86b8000aa78005374
ethanr04 <na23961@bristol.ac.uk>
2025-02-11 19:29:29 +0000

    Added test for footer - specifically logo and motto

--- src/tests/choose-word.test.js
+++ src/tests/choose-word.test.js
@@ -8,6 +8,13 @@ test("Checks page is rendered with the correct title and icon", async () => {
     expect(res.text).toEqual(expect.stringContaining('<link rel="icon" href="/images/icon.png" sizes="64x64">')); // Check icon is included in response
 });
 
+test("Checks footer contains the logo and motto", async () => {
+    const res = await request(app).get("/mood_summary");
+    expect(res.status).toBe(200);
+    expect(res.text).toContain('<img class="logo" src="images/logo.png" alt="Raymer Enterprises Ltd"');
+    expect(res.text).toContain('<span class="motto mouseM">With emotional health in mind</span>');
+});
+
 afterAll(() => {
     server.close(); // Close the server after the tests are done
   });
