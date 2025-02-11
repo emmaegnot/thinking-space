5a2c84451eae11ebbf3452a91442d3300ffa3d7b
ethanr04 <na23961@bristol.ac.uk>
2025-02-11 19:27:51 +0000

    Added choose-word.test.js, included basic title, icon and footer tests

---
+++ src/tests/choose-word.test.js
@@ -0,0 +1,13 @@
+const request = require("supertest");
+const {server, app} = require("../server/server");
+
+test("Checks page is rendered with the correct title and icon", async () => {
+    const res = await request(app).get("/choose_colour") // Make request to server
+    expect(res.statusCode).toBe(200); // Status code 200 indicates a successful request and response
+    expect(res.text).toContain("<title>The Thinking Space | Choose A Colour</title>"); // Checks dynamic title
+    expect(res.text).toEqual(expect.stringContaining('<link rel="icon" href="/images/icon.png" sizes="64x64">')); // Check icon is included in response
+});
+
+afterAll(() => {
+    server.close(); // Close the server after the tests are done
+  });
