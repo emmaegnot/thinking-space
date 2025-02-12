42701bcb34691bab1462d7130bb73127044b577a
ethanr04 <na23961@bristol.ac.uk>
2025-02-12 10:16:48 +0000

    Create feeling-force.test.js, add title and logo to feeling_force.ejs, add base tests

---
+++ src/tests/feeling-force.test.js
@@ -0,0 +1,20 @@
+const request = require("supertest");
+const {server, app} = require("../server/server");
+
+test("Checks page is rendered with the correct title and icon", async () => {
+    const res = await request(app).get("/feeling_force") // Make request to server
+    expect(res.statusCode).toBe(200); // Status code 200 indicates a successful request and response
+    expect(res.text).toContain("<title>The Thinking Space | Feeling Force</title>"); // Checks dynamic title
+    expect(res.text).toEqual(expect.stringContaining('<link rel="icon" href="/images/icon.png" sizes="64x64">')); // Check icon is included in response
+});
+
+test("Checks footer contains the logo and motto", async () => {
+    const res = await request(app).get("/feeling_force");
+    expect(res.status).toBe(200);
+    expect(res.text).toContain('<img class="logo" src="images/logo.png" alt="Raymer Enterprises Ltd"');
+    expect(res.text).toContain('<span class="motto mouseM">With emotional health in mind</span>');
+});
+
+afterAll(() => {
+    server.close(); // Close the server after the tests are done
+  });
